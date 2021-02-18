27465e9661ed44d9198afaef7843ace52d207eaa
arjun-sudo <arjungautam5431@gmail.com>
2021-02-18 12:06:55 +0545

    add user

--- src/main/java/controller/SparkREST.java
+++ src/main/java/controller/SparkREST.java
@@ -6,5 +6,25 @@
  */
 package controller;
 
+import com.google.gson.Gson;
+import model.User;
+import service.UserServiceImpl;
+import status.StandardResponse;
+import status.StatusResponse;
+
+import static spark.Spark.*;
+
 public class SparkREST {
+    public static void main(String[] args) {
+        final UserServiceImpl userService = new UserServiceImpl();
+
+        /* Add user */
+        post("/users", (request, response) -> {
+            response.type("application/json");
+            User user = new Gson().fromJson(request.body(), User.class);
+
+            userService.addUser(user);
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS));
+        });
+    }
 }
